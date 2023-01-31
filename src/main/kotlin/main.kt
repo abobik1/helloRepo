59417273196d1fc3c6cbf5e683d715e2210bb590
1400724 <1400724@bsu.edu.ru>
2023-01-31 18:13:23 +0300

    1

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,29 @@
+fun main(){
+        val employees = listOf(
+            Employee("Tom", "Microsoft"),
+            Employee("Bob", "JetBrains"),
+            Employee("Sam", "Google"),
+            Employee("Alice", "Microsoft"),
+            Employee("Kate", "Google")
+        )
+        val companies = employees.groupBy { it.company }    // объект Map<String, List<Employee>>
+     
+        println(companies) // {Microsoft=[Tom, Alice], JetBrains=[Bob], Google=[Sam, Kate]}
+     
+        // перебор групп
+        for (company in companies){
+                println(company.key) // название компании
+                // перебор списка сотрудников
+                for (employee in company.value){
+                    println(employee.name)
+                }
+                println() // для отделения групп
+            }
+}
+class Employee(val name: String, val company: String){
+        override fun toString(): String = name
+}
+
+
+
+}
